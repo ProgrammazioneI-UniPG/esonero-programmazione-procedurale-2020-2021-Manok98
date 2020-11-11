bc34d1a10c4083d9e5fc030360b93776631407b2
Manok98 <xdadev98@gmail.com>
2020-11-11 17:56:02 +0100

    Added option to see last used strings

--- cifrario.c
+++ cifrario.c
@@ -12,15 +12,18 @@ typedef enum State {Free, Crypting, Decrypting} status;
 //Public variables:
 si sceltaMenus = 0;
 status actualStatus = Free;
-ch stringaCifrata[129];    //129 di cui 128 per la stringa e 1 per il carattere \0
+ch stringaCifrata[129];    //Riserviamo un posto al carattere terminatore \0
 ch chiave[129];
+ch storico[385];     //Una piccola history dell'ultima stringa decifrata. Non più di una stringa però: ricordiamoci che stiamo scrivendo nella RAM e non nella memoria interna!
 int trash;     //Usata per pulizia del buffer
-si sceltaNonValida = 0;    //Prevediamo ogni inserimento dell'utente e non faremo crashare il programma
+si troppoLunga = 0;  //Da usare per il calcolo della lunghezza della stringa in caso di stringa di 128 caratteri
+ch antiBug;
 
 //Prototypes:
 void MenuPrincipale(int );
 void Crypt();
 void Decrypt();
+void LastString();
 
 int main() {
    system("clear");     //Pulizia del terminale per una visione ottimale
@@ -38,22 +41,39 @@ void MenuPrincipale(int caller){
    }//Fine del messaggio di benvenuto
 
    /*Mostra le opzioni*/
-   sceltaMenus = 1;     //Se l'utente inserisci lettere anzichè numeri, si selezionerà la criptazione di una stringa
    printf("\n[\033[1;34m0\033[0m]Esci dal programma.\n");
    printf("[\033[1;34m1\033[0m]Cripta una stringa.\n");
    printf("[\033[1;34m2\033[0m]Decripta una stringa.\n");
+   printf("[\033[1;34m3\033[0m]Storico delle stringhe.\n");
 
    /*Nel caso in cui il programma sia già stato eseguito*/
    if(actualStatus == 1)
       printf("\n\033[0;32mTrovata stringa criptata\033[0m; digitare \033[1;34m2\033[0m per decriptare.\n");
 
-   printf("\nLa tua scelta: \033[1;34m");
-   scanf("%hd", &sceltaMenus);      //Anche se l'utente inserisse un numero oltre il max dello shortint, questo verrebbe tagliato
+   printf("\nLa tua scelta[Invio = 1]: \033[1;34m");
+   scanf("%c", &antiBug);
    printf("\033[0m");      //Ripristina il colore di default
+
+   /*Interpretiamo l'inserimento*/
+   if(antiBug == '\n'){
+      sceltaMenus = 1;     //Se l'utente preme invio senza scrivere, viene interpretato come 1
+   }
+   else if(antiBug < 48 || antiBug > 57)   //I numeri in ascii vanno da 48 a 57, quindi questa condizione equivale a "Se l'utente non inserisce un numero"
+   {
+      printf("\a\033[1;31mData la scelta non valida, si procede \nautomaticamente con l'opzione 1\033[0m.\n");
+      sleep(2);
+      sceltaMenus = 1;
+      while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+   }else{
+      sceltaMenus = (si)antiBug - 48;
+      while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+   }//Fine dell'if per controllo
 
    /*Gestisci l'inserimento*/
    switch(sceltaMenus){
       case 0:     /*Uscita dal programma*/
+      printf("\a\033[1;31mAr\033[1;32mr\033[01;33mi\033[1;34mve\033[1;35mde\033[1;36mrc\033[0mi!\n");
+      sleep(1);
       exit(0);
       break;
 
@@ -63,6 +83,10 @@ void MenuPrincipale(int caller){
 
       case 2:     /*Decripta una stringa*/
       Decrypt();
+      break;
+
+      case 3:
+      LastString();
       break;
 
       default:    /*Numero non valido*/
@@ -93,14 +117,27 @@ void Crypt(){
    }//Fine dell'if
 
    /*Inserimento della stringa*/
+   for(si i = 0; i <= 128; i++){
+      stringaInserita[i] = '\0';
+   } //Pulisce la stringa prima di riempirla, utile soprattutto se il programma sta criptando molte stringhe
    printf("\nInserisci la stringa da criptare: \033[1;36m");
-   while((trash = getchar()) != '\n' && trash != EOF);       //Pulisce il buffer di lettura prima di leggere
-   fgets(stringaInserita, 128, stdin);       //Salva fino a 128 caratteri della stringa nella variabile <stringaInserita>
+   fgets(stringaInserita, 129, stdin);       //Salva fino a 128 caratteri della stringa nella variabile <stringaInserita>, dalla cella 0 a 127
+   troppoLunga = 0; //La chiave è meno di 128 caratteri
+   //Non è necessario pulire il buffer di lettura se questo è già vuoto: dobbiamo verificare se l'utente abbia inserito 128 o più caratteri e in caso pulire!
+   if(stringaInserita[127] != '\n' && stringaInserita[127] != '\0'){   //Avendo pulito il vettore prima di acquisire, ora sappiamo che sull'ultima cella acquisita (127) ci deve essere \0 o al più \n
+      while((trash = getchar()) != '\n' && trash != EOF);      //L'utente ha messo più di 128 caratteri: dobbiamo pulire tutto ciò che è in più!
+      troppoLunga = 1; //Avvisa che la chiave deve essere di 128
+   }else if(stringaInserita[0] == '\n'){
+      printf("\a\033[1;31mStringa troppo breve\033[0m!\n");
+      Crypt(); //Reinserimento della stringa
+   }
    printf("\033[0m");
+
    /*Verifica presenza escape-code to mainMenu*/
+   //WARNING: TO FIX
    char escapeSequence[] = "backToMain";
    si matchCounter = 0;       //Conta le corrispondenze nel for di seguito
-   if(strlen(stringaInserita)  < 10){ //Se ha più di 10 caratteri sicuramente non è l'scape code
+   if(strlen(stringaInserita)  < 11){ //Se ha più di 10 caratteri (e un \n) sicuramente non è l'scape code
       for(int i = 0; i < 10; i++){
          if(escapeSequence[i] == stringaInserita[i])
              matchCounter++;
@@ -118,35 +155,49 @@ void Crypt(){
       printf("[\033[1;34m0\033[0m]Torna al menu.\n");
       printf("[\033[1;34m1\033[0m]Inserisci chiave.\n");
       printf("[\033[1;34m2\033[0m]Genera chiave.\n");
-      printf("\nInserisci la tua scelta: \033[1;34m");
-      //WARNING: TEST FOR CLEAN NEEDED
-      scanf("%hd", &sceltaMenus);
+      printf("\nLa tua scelta[Invio = 2]: \033[1;34m");
+      scanf("%c", &antiBug);
       printf("\033[0m");      //Ripristina il colore di default
 
-      /*Gestisci l'inserimento*/
+      /*Interpretiamo l'inserimento*/
+      if(antiBug == '\n'){
+         sceltaMenus = 2;     //Se l'utente preme invio senza scrivere, viene interpretato come 2
+      }
+      else if(antiBug < 48 || antiBug > 57){   //I numeri in ascii vanno da 48 a 57, quindi questa condizione equivale a "Se l'utente non inserisce un numero"
+         printf("\a\033[1;31mData la scelta non valida, si procede \nautomaticamente con l'opzione 2\033[0m.\n");
+         sleep(2);
+         sceltaMenus = 2;
+         while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+      }else{
+         sceltaMenus = (si)antiBug - 48;
+         while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+      }//Fine dell'if per controllo
+
+      /*Gestisci l'inserimento della scelta della chiave*/
       switch(sceltaMenus){
          case 0:        /*Torna al menu principale*/
          MenuPrincipale(1);
          break;
 
          case 1:        /*Inserisci una chiave manualmente*/
-         sceltaNonValida = 0;       //Scelta qua sotto: inizializzazione della variabile di controllo
+         for(si i = 0; i <= 128; i++){
+            chiave[i] = '\0';
+         }//Pulizia della chiave
          do{
-         printf("Digita di seguito la chiave (almeno %ld caratteri): \033[1;36m", strlen(stringaInserita) - 1);
-         if(sceltaNonValida == 0)      //Necessario: se il ciclo venisse ripetuto, il buffer cleaning non deve essere ripetuto o impallerà il programma!
-            while((trash = getchar()) != '\n' && trash != EOF);       //Pulisce il buffer di lettura prima di leggere
-         else
-            sceltaNonValida = 0; //Ripristina lo stato
-         fgets(chiave, 128, stdin);       //Salva fino a 128 caratteri della stringa nella variabile <chiave>
-         printf("\033[0m");      //Ripristina il colore di default
-         strlen(chiave) < strlen(stringaInserita) ? printf("\a\n\033[1;31mLa chiave inserita è troppo corta\033[0m;\n"), sceltaNonValida = 1 : printf("La chiave è \033[1;36m%s\033[0m\n", chiave);
+         troppoLunga == 0 ? printf("Digita di seguito la chiave (almeno %ld caratteri): \033[1;36m", strlen(stringaInserita) - 1) : printf("Digita di seguito la chiave (almeno %ld caratteri): \033[1;36m", strlen(stringaInserita));
+         fgets(chiave, 129, stdin);       //Salva fino a 128 caratteri della stringa nella variabile <chiave>
+         if(chiave[127] != '\n' && chiave[127] != '\0'){
+            while((trash = getchar()) != '\n' && trash != EOF);   //Pulisce i caratteri in più
+         }
+         printf("\033[0m");
+         strlen(chiave) < strlen(stringaInserita) ? printf("\a\n\033[1;31mLa chiave inserita è troppo corta\033[0m;\n") : printf("La chiave è \033[1;36m%s\033[0m\n", chiave);
          }while(strlen(chiave) < strlen(stringaInserita));  //Fine del controllo della chiave
          printf("\a\033[1;31mAttenzione\033[0m: essendo stata inserita manualmente la chiave, la\nstringa cifrata potrebbe contenere molti caratteri insoliti.\n");
          sleep(2);
          break;
 
          default:       /*Scelta non valida*/
-         printf("\a\033[1;31mScelta non valida\033[0m! Verrà generata \nuna chiave casuale.\n");
+         printf("\a\033[1;31mScelta non valida\033[0m! Verrà generata una chiave\n casuale. ");
          sleep(2);      //Lascia il tempo di leggere il messaggio
          //Omettendo il break, si eseguirà il case 2, cioè la generazione casuale della chiave, evitando di ripetere i comandi!
 
@@ -154,22 +205,41 @@ void Crypt(){
          printf("Generazione della chiava in maniera casuale...\n");
          time_t t;
          srand((unsigned) time(&t));   //Inizializzato il generatore random
+         for(si i = 0; i <= 128; i++){
+            chiave[i] = '\0';
+         }  //Pulisce la chiave precedente (eventuale)
          for(si i = 0; i < strlen(stringaInserita) - 1; i++){     //Con "-1" escludiamo il "\n"!
             chiave[i] = rand() % 96 + 32;    //Vengono esclusi i caratteri speciali per una stringa cifrata più visual-friendly
          } //Fine della generazione della chiave
-         printf("La chiave è \033[1;36m%s\033[0m.\n", chiave);
+         printf("La chiave è \033[1;36m%s\033[0m\n", chiave);
          break;
 
       }//Fine del switch
 
+      /*Salvataggio della chiave nella hystory*/
+      for(si i = 128; i < 256; i++){  //Pulizia
+         storico[i] = '\0';
+      }
+      for(si i = 0; i < strlen(chiave); i++){   //Scrittura
+         storico[i + 128] = chiave[i];
+      }
       /*Processo di criptazione*/
       actualStatus = Crypting;      //Comunica al resto del programma che è iniziato un processo di criptazione
-      printf("Avvio della criptazione...\n");
-      for(si i = 0; i < strlen(stringaInserita); i++){      //WARNING: ESCLUDERE IL "\n" DALLA CIFRATURA?
+      for(si i = 0; i <= 128; i++){
+         stringaCifrata[i] = '\0';
+      }//Pulisce dalla precedente (eventuale) stringa criptata
+      for(si i = 0; i < strlen(stringaInserita); i++){      //Si esclude con lo strettamente minore il carattere \n
          stringaCifrata[i] = stringaInserita[i] ^ chiave[i];
       }//Fine del for per la criptazione
       sleep(2);      //Pausa ad effetto
       printf("\n\a>>La stringa è stata criptata: \n\033[1;36m%s\033[0m\n", stringaCifrata);     //Si va a capo perchè se venisse generato il carattere backspace, la stringa criptata coprirebbe il testo.
+      /*Salvataggio della stringa cifrata*/
+      for(si i = 0; i < 128; i++){  //Pulizia
+         storico[i] = '\0';
+      }
+      for(si i = 0; i < strlen(stringaCifrata); i++){   //Scrittura
+         storico[i] = stringaCifrata[i];
+      }
       printf("Avvio del menu principale...\n");
       sleep(6);      //Diamo all'utente il tempo di leggere
       MenuPrincipale(1);
@@ -181,8 +251,9 @@ void Crypt(){
 void Decrypt(){
    //Private variables:
    ch stringaDecifrata[129];
-   ch confirm = 'Y'; //Variabile per confermare la decriptazione
+   ch confirm = 'S'; //Variabile per confermare la decriptazione
 
+   system("clear");
    /*Verifica della possibilità di procedere*/
    printf("Controllo della presenza di stringhe decriptabili...\n");
    sleep(1);
@@ -196,16 +267,39 @@ void Decrypt(){
       printf("\033[0;32mTrovata stringa criptata\033[0m!\n");
       do{
          printf("Procedere?[S/N] ");
-         while((trash = getchar()) != '\n' && trash != EOF);
-         scanf("%c", &confirm);
+         scanf("%c", &antiBug);
+
+         /*Interpretiamo l'inserimento*/
+         if(antiBug == '\n'){
+            confirm = 'S';     //Se l'utente preme invio senza scrivere, viene interpretato come Y
+            printf("\a\033[1;31mChi tace acconsente\033[0m.\n");     //Faccio finta che non hai fatto apposta
+         }
+         else if(antiBug != 83 && antiBug != 115 && antiBug != 78 && antiBug != 110)
+         {
+            confirm = 'e'; //e = errore, carattere non valido
+            while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+         }else{
+            confirm = (char)antiBug;
+            while((trash = getchar()) != '\n' && trash != EOF); //Il buffer contiene ancora almeno un \n sicuramente: puliamo
+         }//Fine dell'if per controllo
          if(confirm == 'S' || confirm == 's'){
+            for(si i = 0; i < 128; i++){
+               stringaDecifrata[i] = '\0';
+            }  //Pulisce dalle precedenti stringhe
             printf("Avvio della decriptazione...\n");
             actualStatus = 2;    //Imposta lo stato di decriptazione
-            for(si i = 0; i < strlen(stringaCifrata); i++){
+            for(si i = 0; i <= strlen(stringaCifrata); i++){   //Minore e uguale per non escludere l'ultimo carattere della stringa
                stringaDecifrata[i] = stringaCifrata[i] ^ chiave[i];
             }
             sleep(2);      //Solita pausa ad effetto
             printf("\n\a>>La stringa decriptata è \033[1;36m%s\033[0m\n", stringaDecifrata);
+            /*Salvataggio della stringa*/
+            for(si i = 256; i <= 385; i++){  //Pulizia
+               storico[i] = '\0';
+            }
+            for(si i = 0; i < strlen(stringaDecifrata); i++){   //Scrittura
+               storico[i + 256] = stringaDecifrata[i];
+            }
             printf("Avvio del menu principale...\n");
             sleep(6);
             MenuPrincipale(1);
@@ -217,3 +311,58 @@ void Decrypt(){
    }//Fine dell'else
 
 }//Fine di Decrypt
+
+void LastString(){
+   //Private variables:
+   ch choice[2] = {'\0', '\0'};
+
+   system("clear");
+   printf("\033[01;33mStorico stringhe\033[0m:\n");
+   printf("Ultima stringa cifrata: \n\t\033[1;36m");
+   /*Stampiamo la stringa cifrata*/
+   if(storico[0] == '\0'){
+      printf("\033[1;31mNessun dato ancora\033[0m!");
+   }else{
+      for(si i = 0; i < 128; i++){
+         if(storico[i] != '0')
+            printf("%c", storico[i]);
+         else
+            break;
+      }
+   }
+   printf("\n\033[0mUltima chiave usata: \n\t\033[1;36m");
+   /*Stampiamo la chiave*/
+   if(storico[128] == '\0'){
+      printf("\033[1;31mNessun dato ancora\033[0m!");
+   }else{
+      for(si i = 128; i < 256; i++){
+         if(storico[i] != '0')
+            printf("%c", storico[i]);
+         else
+            break;
+      }
+   }
+   printf("\n\033[0mUltima stringa decifrata: \n\t\033[1;36m");
+   /*Stampiamo la stringa decifrata*/
+   if(storico[256] == '\0'){
+      printf("\033[1;31mNessun dato ancora\033[0m!");
+   }else{
+      for(si i = 256; i < 384; i++){
+         if(storico[i] != '0')
+            printf("%c", storico[i]);
+         else
+            break;
+      }
+   }
+   printf("\n\n\033[0mPremere Invio per tornare al menu. ");
+   fgets(choice, 3, stdin);
+   if(choice[0] == '\n'){
+      MenuPrincipale(1);
+   }else if(choice[1] != '\0' && choice[1] != '\n'){
+      //Torniamo al menu lo stesso, ma puliamo il buffer
+      while((trash = getchar()) != '\n' && trash != EOF);
+      MenuPrincipale(1);
+   }else{   //Prima di premere invio ha premuto un tasto...fa niente
+      MenuPrincipale(1);
+   }
+}//Fine di LastString
